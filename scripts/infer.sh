46faaa6b9819100494dab7186e012752b0172860
mrlasdt <mrlasdt@gmail.com>
2023-07-12 09:40:20 +0000

    update requirement.txt and add infer script

---
+++ scripts/infer.sh
@@ -0,0 +1,5 @@
+python inference.py --driven_audio /home/ubuntu/hungbnt/SadTalker/examples/Buddhism_Domi.mp3 \
+                    --source_video /home/ubuntu/hungbnt/SadTalker/examples/0709.mp4 \
+                    #--enhancer face \
+                    #--use_DAIN \
+                    #--time_step 0.5 
